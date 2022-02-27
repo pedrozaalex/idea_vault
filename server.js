330b1cd1398d8fcfcf8dd0dd14cbb0819bd10537
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2022-02-26 23:02:07 -0300

    :see_no_evil: add .idea to .gitignore

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createEventHandler } from '@remix-run/cloudflare-workers';
-import * as build from '@remix-run/dev/server-build';
+import { createRequestHandler } from "@remix-run/vercel";
+import * as build from "@remix-run/dev/server-build";
 
-addEventListener('fetch', createEventHandler({ build, mode: process.env.NODE_ENV }));
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
